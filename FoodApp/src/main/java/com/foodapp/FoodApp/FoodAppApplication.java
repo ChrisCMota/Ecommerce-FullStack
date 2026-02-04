bdda386e6c99e89bc5ddb571b0f844852ba3ea4f
ChrisCMota <100359859+ChrisCMota@users.noreply.github.com>
2026-02-04 10:28:09 +0000

    Email sender tested and working

--- FoodApp/src/main/java/com/foodapp/FoodApp/FoodAppApplication.java
+++ FoodApp/src/main/java/com/foodapp/FoodApp/FoodAppApplication.java
@@ -2,12 +2,33 @@ package com.foodapp.FoodApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+//@RequiredArgsConstructor -- TESTING EMAIL SENDER
 public class FoodAppApplication {
+
+//	private final INotificationService notificationService; -- TESTING EMAIL SENDER
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodAppApplication.class, args);
 	}
+
+
+//-- TESTING EMAIL SENDER	
+//	@Bean
+//	CommandLineRunner runner(){
+//		return args -> {
+//			NotificationDTO notificationDTO = NotificationDTO.builder()
+//					.recipient("christian.kfsn@yahoo.com.br")
+//					.subject("Sent from foodapp api - Testing !!!!")
+//					.body("This is an API test !!!!!")
+//					.type(NotificationType.EMAIL)
+//					.build();
+//
+//			notificationService.sendEmail(notificationDTO);
+//		};
+//	}
 
 }
