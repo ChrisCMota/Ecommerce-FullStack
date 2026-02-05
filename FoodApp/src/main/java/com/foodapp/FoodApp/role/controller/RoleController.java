17321a2e6e66764c1f6075c39c36abb99df47373
ChrisCMota <100359859+ChrisCMota@users.noreply.github.com>
2026-02-05 13:54:25 +0000

    Role Controller added

---
+++ FoodApp/src/main/java/com/foodapp/FoodApp/role/controller/RoleController.java
@@ -0,0 +1,42 @@
+package com.foodapp.FoodApp.role.controller;
+
+import com.foodapp.FoodApp.response.Response;
+import com.foodapp.FoodApp.role.dtos.RoleDTO;
+import com.foodapp.FoodApp.role.service.IRoleService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/roles")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class RoleController {
+
+    private final IRoleService roleService;
+
+    @PostMapping
+    public ResponseEntity<Response<RoleDTO>> createRole(@RequestBody @Valid RoleDTO roleDTO){
+        return ResponseEntity.ok(roleService.createRole(roleDTO));
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Response<RoleDTO>> updateRole(@RequestBody @Valid RoleDTO roleDTO){
+        return ResponseEntity.ok(roleService.updateRole(roleDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Response<List<RoleDTO>>> getAllRoles(){
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Response<?>> deleteRole(@PathVariable @Valid Long id){
+        return ResponseEntity.ok(roleService.deleteRole(id));
+    }
+
+}
